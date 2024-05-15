6e5b11c75322040f8a672d382d6921eb4a8239b6
Venkatesan N <akashvenkat47@gmail.com>
2024-05-16 01:40:24 +0530

    final build

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDX8nNcFQpjgg3KeQaPSRU8H3A69DOyHbE",
-  authDomain: "crypto-navigator-cd8dd.firebaseapp.com",
-  projectId: "crypto-navigator-cd8dd",
-  storageBucket: "crypto-navigator-cd8dd.appspot.com",
-  messagingSenderId: "209501739343",
-  appId: "1:209501739343:web:2de66be03443f54442cd3a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
